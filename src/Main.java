11fbf78df53aaf3b4aaa16725a3ab0fe247bc35b
BT_1N3_19 <email@gmail.com>
2025-01-10 13:54:36 -0500

    added line

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hi and goodbye");
         System.out.println("yo");
         System.out.println("line :D");
         System.out.println("more linessss");
